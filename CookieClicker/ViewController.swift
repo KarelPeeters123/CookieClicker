3da7633f3926744dd23ed06b2f2d988f2681eae1
Karel <Karel.Peeters@ciminko.lu>
2021-03-28 09:34:09 +1300

    Initial Commit

---
+++ CookieClicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CookieClicker
+//
+//  Created by Karel Peeters on 28/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
